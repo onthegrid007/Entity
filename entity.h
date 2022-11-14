dc63e2b6ba136f5610f9999baf1203054679dafa
onthegrid007 <peter.ferranti.dev@gmail.com>
2022-11-14 16:53:11 -0500

    .

--- entity.h
+++ entity.h
@@ -2,7 +2,7 @@
 #define ENTITY_H
 
 #include "vendor/CPPLOG/vendor/LifetimeEvent/vendor/UUID/UUID.h"
-#include "vendor/SI/include/SI/mass.h"
+#include "vendor/SI/include/SI/astronomic.h"
 #include "vendor/SI/include/SI/velocity.h"
 #include "vendor/glm/glm/glm.hpp"
 #include "vendor/glm/glm/mat4x4.hpp"
@@ -23,7 +23,6 @@ class Entity {
     dvec3 m_rotation = {0, 0, 0};
     MassType m_mass = 1.0_kg;
     VelocityType m_velocity = 0.0_m_p_s;
-    // bool m_physicsAffected = true;
     
     protected:
     UUID m_UUID;
